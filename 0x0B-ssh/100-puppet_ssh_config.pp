94b32eea1ba2777e4898dff15d17905e0feead55
tabi5 <tinbitdemissie@gmail.com>
2024-01-23 06:27:21 -0800

    puppet

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+# use puppet to make changes to configuration
+
+file { 'ect/ssh/ssh_cofig':
+        ensure => present,
+
+content =>"
+
+	#SSH client configuration
+	host"
+	IdentityFile ~/.ssh/school
+	PasswordAuthentication no
+
+
+}
